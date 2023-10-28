9a311c494b410fc1c069e66938288a93c684a9b9
igor-magno <igormagno.dev@gmail.com>
2023-10-28 13:21:28 -0300

    feat: :construction: dump implementation of the movies catalog, integrated with rapid api.

---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ __('Movies') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="antialiased">
+    <div class="w-screen h-screen flex justify-center items-center space-x-4">
+        <a href="{{ route('movie.paginate-scroll') }}" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">{{ __('Movies in grid scroll') }}</a>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,50 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('movie.index');
+});
+
+Route::get('/paginate-scroll', function () {
+    return view('movie.paginate-scroll');
+})->name('movie.paginate-scroll');
+
+Route::get('/movie', function (Request $request) {
+    $page = $request->get('page') ?? 1;
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "https://moviesdatabase.p.rapidapi.com/titles?page={$page}",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "X-RapidAPI-Host: " . env('X_RapidAPI_Host'),
+            "X-RapidAPI-Key: " . env('X_RapidAPI_Key')
+        ],
+    ]);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    }
+    $response = json_decode($response);
+    $movies = array_map(function ($movie) {
+        return (object)[
+            'cover' => $movie->primaryImage ? (object)[
+                'url' => $movie->primaryImage->url,
+                'width' => $movie->primaryImage->width ?? null,
+                'height' => $movie->primaryImage->height ?? null,
+            ] : null,
+            'title' => $movie->titleText->text ?? $movie->originalTitleText->text ?? '',
+            'year' => $movie->releaseYear->year ?? ''
+        ];
+    }, $response->results);
+    return response()->json((object)[
+        'movies' => $movies,
+        'requestPage' => $page,
+        'nextPage' => $page + 1
+    ], 200);
 });
